9d24e80e1732da73b2e9437fadcacd02105da3a1
Iván Rodríguez Murillo <wantez@gmail.com>
2015-08-14 22:49:55 +0200

    Seed data update

--- server/config/seed.js
+++ server/config/seed.js
@@ -5,6 +5,8 @@
 
 'use strict';
 
+var _ = require('lodash');
+
 var User = require('../api/user/user.model');
 var Service = require('../api/service/service.model');
 
@@ -24,6 +26,16 @@ User.find({}).remove(function () {
       console.log('finished populating users');
     }
   );
+
+  for (var i = 0; i < 500; i++) {
+    User.create({
+      provider: 'local',
+      name: 'Test User ' + i,
+      email: i + 'test@test.com',
+      password: 'test'
+    });
+  }
+
 });
 
 Service.find({}).remove(function () {
@@ -31,19 +43,34 @@ Service.find({}).remove(function () {
       name: 'Sample Service',
       code: 'sampleservice',
       public: true,
+      example: true,
       hits: 10,
       latestVersion: '2.1.0',
+      defaultHeaders: [
+        {name: 'api_key', value: 'test-key'}
+      ],
       endpoints: [
-        {uri: 'http://localhost:9000/api/samples/v1', hits: 4, apiVersion: '1.0.1'},
-        {uri: 'http://localhost:9000/api/samples/v2', hits: 1, apiVersion: '2.0.0'},
-        {uri: 'http://localhost:9000/api/samples/v2', hits: 3, apiVersion: '2.0.3'},
-        {uri: 'http://localhost:9000/api/samples/v2', hits: 2, apiVersion: '2.1.0'}
+        {
+          uri: 'http://localhost:9000/api/samples/v101', apiDoc: 'swagger.json', hits: 4, apiVersion: '1.0.1'
+        },
+        {uri: 'http://localhost:9000/api/samples/v200', apiDoc: 'swagger.json', hits: 1, apiVersion: '2.0.0'},
+        {uri: 'http://localhost:9000/api/samples/v203', apiDoc: 'swagger.json', hits: 3, apiVersion: '2.0.3'},
+        {
+          uri: 'http://localhost:9000/api/samples/v210',
+          apiDoc: 'swagger.json',
+          hits: 2,
+          apiVersion: '2.1.0',
+          headers: [
+            {name: 'newextraapi_key', value: 'newtest-key'}
+          ]
+        }
       ]
     }, {
       name: 'Swagger Petstore',
       code: 'swaggerpetstore',
       public: true,
       hits: 0,
+      latestVersion: '2',
       endpoints: [
         {
           uri: 'http://petstore.swagger.io/v2',
@@ -54,9 +81,24 @@ Service.find({}).remove(function () {
           ]
         }
       ]
+    }, {
+      name: '\';--have i been pwned?',
+      code: 'haveibeenpwned',
+      public: true,
+      hits: 0,
+      latestVersion: '2',
+      endpoints: [
+        {
+          uri: 'https://haveibeenpwned.com/api/v2/',
+          apiVersion: '2',
+          apiDoc: ''
+        }
+      ]
     },
     function () {
       console.log('finished populating services and endpoints');
     }
   );
 });
+
+
