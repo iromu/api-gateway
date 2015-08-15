efc88361fa4b21b7bd686abd285ae17c60400fdc
Iván Rodríguez Murillo <wantez@gmail.com>
2015-08-15 21:05:48 +0200

    karma tunning

--- karma.conf.js
+++ karma.conf.js
@@ -25,21 +25,14 @@ module.exports = function (config) {
       'client/bower_components/restangular/dist/restangular.js',
       'client/bower_components/angular-swagger-ui/dist/scripts/swagger-ui.min.js',
       'client/app/app.js',
-      'client/app/app.coffee',
       'client/app/**/*.js',
-      'client/app/**/*.coffee',
       'client/components/**/*.js',
-      'client/components/**/*.coffee',
-      'client/app/**/*.jade',
-      'client/components/**/*.jade',
       'client/app/**/*.html',
       'client/components/**/*.html'
     ],
 
     preprocessors: {
-      '**/*.jade': 'ng-jade2js',
-      '**/*.html': 'html2js',
-      '**/*.coffee': 'coffee'
+      '**/*.html': 'html2js'
     },
 
     ngHtml2JsPreprocessor: {
