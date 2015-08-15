76298a0d8e32507b8eb78787c6795a68dab98793
Iván Rodríguez Murillo <wantez@gmail.com>
2015-08-15 23:47:40 +0200

    Jumbotron asset

--- Gruntfile.js
+++ Gruntfile.js
@@ -380,6 +380,7 @@ module.exports = function (grunt) {
               'bower_components/**/*',
               'assets/images/{,*/}*.{webp}',
               'assets/fonts/**/*',
+              'assets/templates/**/*',
               'index.html'
             ]
           }, {
