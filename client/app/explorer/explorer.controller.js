9d0703ced9342c0ea7c9e9d23c7f87148fbf5b71
Iván Rodríguez Murillo <wantez@gmail.com>
2015-08-18 19:04:49 +0200

    Typeahead

--- client/app/explorer/explorer.controller.js
+++ client/app/explorer/explorer.controller.js
@@ -4,16 +4,16 @@
   angular.module('app')
     .controller('ExplorerController', ExplorerController);
 
-  function ExplorerController($scope, $stateParams, $location) {
-    var vm = this;
-    vm.apiVersionSelection = '@latest';
+  function ExplorerController($scope, $stateParams, $location, FullRestangular) {
 
     console.log('Init ' + JSON.stringify($stateParams));
 
-    var port = (($location.port() !== 80) ? ':' + $location.port() : '');
-    vm.apiRestBaseEndpoint = $location.protocol() + '://' + $location.host() + port + '/';
+    var vm = this;
+    vm.loadingServices = false;
+    vm.apiVersionSelection = '@latest';
 
-    activate();
+    var port = ($location.port() !== 80 && $location.protocol() !== 'https') ? ':' + $location.port() : '';
+    vm.apiRestBaseEndpoint = $location.protocol() + '://' + $location.host() + port + '/';
 
     // error management
     vm.swaggerErrorHandler = function (data, status) {
@@ -25,12 +25,33 @@
       request.headers['X-Api-Version'] = vm.apiVersionSelection;
     };
 
-    function exploreSwagger() {
-      var ts = Date.now();
-      vm.swaggerUrl = '/' + vm.codeSelection + '/swagger.json' + '?X-Api-Version=' + vm.apiVersionSelection + '&ts=' + ts;
-      vm.swaggerUrlMin = '/' + vm.codeSelection + '/swagger.json' + '?X-Api-Version=' + vm.apiVersionSelection;
-      vm.swaggerUrlDisplay = 'http://' + $location.host() + '/' + vm.codeSelection + '/swagger.json';
+
+    vm.loadVersions = function () {
+      console.log('loadVersions');
+      FullRestangular.all('services').getList({code: vm.codeSelection}).then(function (response) {
+        console.log('ExplorerController services by code');
+        $scope.service = response.data[0];
+        vm.endpoints = $scope.service.endpoints;
+      });
     };
+
+    vm.selectVersion = function (version) {
+      vm.apiVersionSelection = version;
+      exploreSwagger();
+    };
+
+
+    vm.getServices = function (text) {
+      FullRestangular.all('services').getList({code: text, typeahead: true}).then(function (response) {
+        var codes = [];
+        angular.forEach(response.data, function (service) {
+          codes.push(service.code);
+        });
+        return codes;
+      });
+    };
+
+    activate();
 
     $scope.$on('$destroy', function () {
       console.log('Destroy explorer');
@@ -43,8 +64,16 @@
       $scope.$emit('showExplorer', true);
       if ('code' in $stateParams) {
         vm.codeSelection = $stateParams.code;
+        vm.loadVersions();
         exploreSwagger();
       }
+    }
+
+    function exploreSwagger() {
+      var ts = Date.now();
+      vm.swaggerUrlDisplay = vm.apiRestBaseEndpoint + vm.codeSelection + '/swagger.json';
+      vm.swaggerUrl = vm.swaggerUrlDisplay + '?X-Api-Version=' + vm.apiVersionSelection + '&ts=' + ts;
+      vm.swaggerUrlMin = '/' + vm.codeSelection + '/swagger.json' + '?X-Api-Version=' + vm.apiVersionSelection;
     }
   }
 }());
