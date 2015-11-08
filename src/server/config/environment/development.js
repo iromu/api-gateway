3d6d303ec43d791786b32db07373b065518517d7
Iván Rodríguez Murillo <wantez@gmail.com>
2015-11-08 12:58:39 +0100

    bower update

--- src/server/config/environment/development.js
+++ src/server/config/environment/development.js
@@ -7,5 +7,9 @@ module.exports = {
   mongo: {
     uri: process.env.MONGODB_URL || 'mongodb://localhost/apigateway-dev'
   },
+
+  redis: {
+    uri: process.env.REDIS_URL || 'redis://localhost:6379'
+  },
   seedDB: true
 };
